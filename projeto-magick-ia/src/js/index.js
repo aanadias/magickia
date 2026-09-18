c83ca59a0ffb70b85f633ede58832f2ace45d296
Ana Clara Gomes Dias <aanagomesdias@gmail.com>
2026-09-18 10:45:42 -0300

    Update index.js

--- projeto-magick-ia/src/js/index.js
+++ projeto-magick-ia/src/js/index.js
@@ -1,13 +1,13 @@
-// passo 1 - pegar o botao de aplicar filtros do HTML e mandar pro JS
+
 const botaoFiltrar = document.querySelector(".btn-filtrar");
 
-// passo 2 - escutar o clique no botão de aplicar filtros
+
 botaoFiltrar.addEventListener("click", function () {
-  // passo 3 - pegar os valores dos campos de categoria e preço
+
   const categoriaSelecionada = document.querySelector("#categoria").value;
   const precoMaximoSelecionado = document.querySelector("#preco").value;
 
-  // passo 4 - para cada carta, verificar se ela deve ser mostrada ou escondida baseado nos filtros que a pessoa digitou
+
   const cartas = document.querySelectorAll(".carta");
 
   cartas.forEach(function (carta) {
